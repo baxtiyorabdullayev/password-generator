9f41f32f2e6cae903df4c3a0d0f38657dc364de7
Bakhtiyor Abdullaev <bakhtiyor.abdullaev.frontend@gmail.com>
2023-04-07 12:00:05 +0500

    html elements creating function and append child functions created

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
-function generatePassword() {
-  var length = 8,
-    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-    retVal = "";
-  for (var i = 0, n = charset.length; i < length; ++i) {
-    retVal += charset.charAt(Math.floor(Math.random() * n));
-  }
-  return retVal;
+"use strict";
+
+let body = document.querySelector("body");
+
+// elementName, tagName, elementClass, text
+let box = elementCreator("box", "div", "bigBox", "This is container");
+let btn = elementCreator("btn", "button", "clickButton", "click me");
+
+// function generatePassword() {
+//   var length = 8,
+//     charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+//     retVal = "";
+//   for (var i = 0, n = charset.length; i < length; ++i) {
+//     retVal += charset.charAt(Math.floor(Math.random() * n));
+//   }
+//   return retVal;
+// }
+// console.log(generatePassword());
+
+// helper functions **********************************************
+
+// HTML element creator function
+function elementCreator(elementName, tagName, elementClass, text) {
+  let foo = (elementName = document.createElement(tagName));
+  elementName.className = elementClass;
+  elementName.textContent = text;
+  return foo;
 }
-console.log(generatePassword());
+
+// append childer to html
+function appendChilder(father, child) {
+  return father.appendChild(child);
+}
